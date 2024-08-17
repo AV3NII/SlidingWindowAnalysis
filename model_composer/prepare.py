7b3942dc5a65455152bd38edd6cca37b7e64ab59
Veit Brunnhuber <vxbdeveloper@gmail.com>
2024-08-17 20:05:23 +0200

    prepare cnn

--- model_composer/prepare.py
+++ model_composer/prepare.py
@@ -39,4 +39,15 @@ def prepare_for_model(
 
         model = model_func(input_shape)
 
+    elif 'cnn' in model_name:
+        input_shape = (window_size, len(feature_names), 1)
+        print(f"Input shape for {model_name}: {input_shape}")
+
+        # Convert DataFrames to numpy arrays and reshape for CNN models
+        x_train = x_train.values.reshape(-1, window_size, len(feature_names), 1).astype(np.float32)
+        x_val = x_val.values.reshape(-1, window_size, len(feature_names), 1).astype(np.float32)
+        x_test = x_test.values.reshape(-1, window_size, len(feature_names), 1).astype(np.float32)
+
+        model = model_func(input_shape)
+
     return model, x_train, y_train, x_val, y_val, x_test, y_test
