1b89419ea0870bbfe905cca22e75cc63c84eaa8b
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-08-18 14:57:56 +0200

    adapted data loader for rnn

--- helpers/data_loader.py
+++ helpers/data_loader.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
 from helpers.prepocess_data import preprocess_df
 import pandas as pd
 
@@ -46,5 +49,50 @@ def split_time_series_data(df, target_column, val_size=0.15, test_size=0.15, win
     x_train, y_train = create_features(train_data)
     x_val, y_val = create_features(val_data)
     x_test, y_test = create_features(test_data)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+def prepare_for_rnn(df, target_column, val_size=0.15, test_size=0.15, window_size=1):
+    # Preprocess the DataFrame
+    df = preprocess_df(df)
+
+    # Calculate sizes
+    train_size = 1 - val_size - test_size
+    total_size = len(df)
+    train_end = int(total_size * train_size)
+    val_end = int(total_size * (train_size + val_size))
+
+    # Split the data
+    train_data = df.iloc[:train_end]
+    val_data = df.iloc[train_end:val_end]
+    test_data = df.iloc[val_end:]
+
+    # process the data
+    def process_data(data):
+        data = data[target_column].values.reshape(-1, 1)
+        x_ = []
+        y_ = []
+        for i in range(window_size, len(data)):
+            x_.append(data[i - window_size:i, 0])
+            y_.append(data[i, 0])
+        return np.array(x_), np.array(y_)
+
+    # for training data (scaling and fittransform, the others will be transformed only)
+    scaper = StandardScaler()
+    x_train, y_train = process_data(train_data)
+    x_train = scaper.fit_transform(x_train)
+
+    # for validation data
+    x_val, y_val = process_data(val_data)
+    x_val = scaper.transform(x_val)
+
+    # for test data
+    x_test, y_test = process_data(test_data)
+    x_test = scaper.transform(x_test)
+
+    # reshape the input data
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
     return x_train, y_train, x_val, y_val, x_test, y_test
